c1e6d4a963b638bd8c5c61dd1535eadcde27e33f
Willow Wu <112401415+Topologytomato@users.noreply.github.com>
2022-12-01 14:19:16 -0500

    Update households_all_year_data.R

--- households_all_year_data.R
+++ households_all_year_data.R
@@ -5,22 +5,26 @@ library(tidyverse)
 
 # the place you save the data
 dir_file <- "/Users/willowwu/Documents/households data/"
-country <- "Paraguay"
+
+# the coutry you need (please enter in lower case)
+country <- "paraguay"
+
 outputdata = data.frame()
 
 for (dirname in dir(dir_file)){ 
   
-  # get the dir
+  # get the fullpath
   mydir <- paste0(dir_file,dirname)
   
   # find certain file
   filename_list <- strsplit(dirname, split = ".",fixed=T)
   
-  if (filename_list[[1]][2] == country){
+  # get corresponding data
+  if (tolower(filename_list[[1]][2]) == tolower(country){
     data <- read_xlsx(mydir)
     data$year <- filename_list[[1]][3]
   outputdata <- bind_rows(outputdata,data)
-    }
+  }
 }
 
 outputdata[is.na(outputdata)] <- 0
@@ -28,5 +32,3 @@ outputdata <- outputdata[,2:dim(outputdata)[2]]
 col_digit <- grep("^[a-zA-Z]", colnames(outputdata),value = TRUE,invert = TRUE)
 col_undigit <- grep("^[a-zA-Z]", colnames(outputdata),value = TRUE,invert = FALSE)
 outputdata <- outputdata[,c(col_undigit,mixedsort(col_digit))]
-
-
